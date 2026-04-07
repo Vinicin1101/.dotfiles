a816222992253b6290cf3dc7e601233076acb84b
Vinicin1101 <vserafim_o@outlook.com>
2026-04-06 21:35:12 -0300

    refactor(waybar): remove scripts obsoletos
    - Deleta scripts de volume, lock screen, temperatura e seletores de wallpaper antigos do diretório do waybar.

--- configs/waybar/scripts/lock.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export WALLPAPER=$(swww query | awk '{print $8}')
-sleep 1s
-exec swaylock -c $HOME/.config/swaylock/config
--- configs/waybar/scripts/temp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "{\"text\": \"$(sensors | awk '{ print $2 }' | grep "°C" | sed -n 's/+//g;p'| sed -n 's/\.[0-9]*//g;p'| tail -n 1)\"}"
